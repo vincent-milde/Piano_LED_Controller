5f02229ec20ada82318ddfd1268f761ba9c19c32
Vincent Milde <vincent.milde@gmx.at>
2026-01-27 22:37:20 +0100

    Testing the p-125 and getting the raw usb data

--- firmware/src/off_mode.cpp
+++
@@ -1,9 +0,0 @@
-#include "off_mode.h"
-#include <FastLED.h>
-
-void OffMode::worker() {
-    FastLED.clear();
-}
-light_mode_t OffMode::getMode() {
-    return _mode;
-}
--- firmware/src/piano_visualizer.cpp
+++
@@ -1,7 +0,0 @@
-#include "piano_visualizer.h"
-#include "EspUsbHost.h"
-PianoVisualizer::PianoVisualizer() {}
-
-void PianoVisualizer::worker() {
-                    
-}
